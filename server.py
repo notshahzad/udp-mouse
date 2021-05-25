8831cea9948c9fc81d25f3ac455e446e3c2ee2de
notshahzad <shahzadakhtar182005@gmail.com>
2021-05-25 15:39:06 +0530

    udpserver

---
+++ server.py
@@ -0,0 +1,33 @@
+import socket
+import win32api
+
+Ip = "192.168.43.86"
+Port = 4000
+
+currentposx, currentposy = win32api.GetCursorPos()
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind((Ip, Port))
+while True:
+    posx = 0
+    posy = 0
+    data, addr = server.recvfrom(1024)
+    data = data.decode()
+    data = data.split(",")
+    if data[0] == "a":
+        tilt_side = data[1]
+        tilt_front = data[2]
+        tilt_side = round(float(tilt_side))
+        tilt_front = round(float(tilt_front))
+        if tilt_side <= -4:
+            posx = 1
+        elif tilt_side >= 4:
+            posx = -1
+        if tilt_front <= -4:
+            posy = 1
+        elif tilt_front >= 4:
+            posy = -1
+        currentposx += posx
+        currentposy += posy
+        win32api.SetCursorPos((currentposx, currentposy))
